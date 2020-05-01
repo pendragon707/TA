033a70a33a18126725e702a4a3a7acf6342451ae
Nonpenguin <pendragon707@mail.ru>
2020-05-01 15:29:49 +0300

    remove some files

--- dbo.View.sql
+++
@@ -1,2 +0,0 @@
-﻿CREATE VIEW [dbo].[Vehicle_view]
-	AS SELECT IdVehicle, KindOfTheVehicle, UsefulLife, TechnicalCondition, LicensePlate, RegionName FROM dbo.[Vehicle] INNER JOIN Regions ON [Regions].IdRegion = [Vehicle].IdRegion
